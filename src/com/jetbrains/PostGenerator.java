032520a9b1291331fe7632f13975c2a5003c5839
LaurWork <46980388+LaurWork@users.noreply.github.com>
2019-05-09 20:50:04 -0400

    Update PostGenerator.java

--- src/com/jetbrains/PostGenerator.java
+++ src/com/jetbrains/PostGenerator.java
@@ -7,46 +7,18 @@ public class PostGenerator {
 
     public PostGenerator() {
         this.keyboard = new Scanner(System.in);
-    }
+    
 
-    public Post generatePost() {
-        User user = createUser();
-        String validate;
-        Post newPost;
-        do {
-            newPost = createPost(user);
-            System.out.println("\n" + newPost.toString() + "\n");
-            System.out.println("Would you like to make a new post now? (Enter \"y\" for yes)");
-            validate = keyboard.nextLine();
-        } while (!validate.toLowerCase().equals("y"));
-        return newPost;
-    }
-
-    public User createUser(){
-        User user = new User();
-        System.out.println("What's your first name?");
-        user.setFirstName(keyboard.nextLine());
-        System.out.println("What's your last name?");
-        user.setLastName(keyboard.nextLine());
-        System.out.println("What's your email address?");
-        user.setEmail(keyboard.nextLine());
-        System.out.println("What would you like your username to be?");
-        user.setUsername(keyboard.nextLine());
-        System.out.println("What's the URL to your avatar picture?");
-        user.setAvatarUrl(keyboard.nextLine());
-        return user;
-    }
-
-
-    public Post createPost (User user){
+    public Post generatePost (){
         Post post = new Post();
+        System.out.println("What's your username?");
+        post.setUser(keyboard.nextLine());
         System.out.println("What would you like to say?");
         post.setContent(keyboard.nextLine());
         System.out.println("Type your URL here.");
         post.setUrl(keyboard.nextLine());
+        post.getID();
         return post;
 
     }
 }
-//        post.getUser();
-//                post.getId();
