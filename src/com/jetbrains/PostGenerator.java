96afa9f4670faf61e0a9e0fb5e791e15d8c3ee7d
LaurWork <46980388+LaurWork@users.noreply.github.com>
2019-05-19 16:59:03 -0400

    Update PostGenerator.java

--- src/com/jetbrains/PostGenerator.java
+++ src/com/jetbrains/PostGenerator.java
@@ -1,24 +1,25 @@
+
 package com.jetbrains;
 import java.util.Scanner;
 
 public class PostGenerator {
 
-    private Scanner keyboard;
+    public Scanner keyboard;
 
     public PostGenerator() {
         this.keyboard = new Scanner(System.in);
-    
+    }
 
-    public Post generatePost (){
-        Post post = new Post();
-        System.out.println("What's your username?");
-        post.setUser(keyboard.nextLine());
-        System.out.println("What would you like to say?");
-        post.setContent(keyboard.nextLine());
-        System.out.println("Type your URL here.");
-        post.setUrl(keyboard.nextLine());
-        post.getID();
-        return post;
+       public Post generatePost(User user) {
+            Post post = new Post();
+            post.setUser(user);
+            System.out.println("What would you like to say?");
+            post.setContent(keyboard.nextLine());
+            System.out.println("Type your URL here.");
+            post.setUrl(keyboard.nextLine());
+            post.getId();
+            return post;
+
+        }
 
     }
-}
